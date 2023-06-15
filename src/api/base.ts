d3222c78e8d993a2e376a1f2ea13ca181d1a76c8
maxglio <massimo.maglio.06@gmail.com>
2023-06-15 14:18:32 +0200

    removed .bz.it and added .com

--- src/api/base.ts
+++ src/api/base.ts
@@ -16,7 +16,7 @@ import { Configuration } from "./configuration";
 // @ts-ignore
 import globalAxios, { AxiosPromise, AxiosInstance } from 'axios';
 
-//export const BASE_PATH = "https://tourism.api.opendatahub.bz.it/".replace(/\/+$/, "");
+//export const BASE_PATH = "https://tourism.api.opendatahub.com/".replace(/\/+$/, "");
 export const BASE_PATH = "https://api.tourism.testingmachine.eu/".replace(/\/+$/, "");
 
 /**
